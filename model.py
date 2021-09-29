a7b360147d4dc4f8f155ec4bdfaaec0e252027c4
Sarthak Kamat <sarthakkamat@gmail.com>
2021-09-29 16:40:53 -0700

    update

--- model.py
+++ model.py
@@ -26,7 +26,7 @@ class AudioToPose(nn.Module):
 
         if encoder_dim == 1:
             sizes = [(cdiv(h, factor),) for factor in self.audio_encoder_down_factors]
-            channels = [factor for factor in [1] + self.channel_factors[:-1]]
+            channels = [factor for factor in self.channel_factors[:-1]]
         else:
             sizes = [(cdiv(h, factor), cdiv(w, factor)) for factor in self.audio_encoder_down_factors]
             channels = [factor for factor in self.channel_factors[1:]]
