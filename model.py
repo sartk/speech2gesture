ab32af6265b9142d673c1ac80a2875c11bca31bf
Sarthak Kamat <sarthakkamat@gmail.com>
2021-10-09 21:25:42 -0700

    model

--- model.py
+++ model.py
@@ -11,15 +11,16 @@ import interp_methods
 
 class AudioToPose(nn.Module):
 
-    def __init__(self, pose_shape: Tuple[int, int], input_shape: Tuple[int, int], encoder_dim=2,
-                 audio_encoder_mults = [1, 2, 1, 2, 1, 2, 1, 1], channel_mults = [1, 2, 1, 2, 1, 1, 1, 1]):
+    def __init__(self, pose_shape: Tuple[int, int], input_shape: Tuple[int, int], encoder_dim=2):
         super(AudioToPose, self).__init__()
 
         pose_dof, frames = pose_shape
         h, w = input_shape # h is time, w is features
 
+        audio_encoder_mults = [1, 2, 1, 2, 1, 2, 1, 1]
+        channel_mults = [1 if encoder_dim == 1 else 64, 2, 1, 2, 1, 1, 1, 1]
         self.encoder_dim = encoder_dim
-        self.audio_encoder_down_factors, self.channel_factors = [1], [64]
+        self.audio_encoder_down_factors, self.channel_factors = [1], [None, [64], [1]][encoder_dim]
         for m, n in zip(audio_encoder_mults, channel_mults):
             self.audio_encoder_down_factors.append(m * self.audio_encoder_down_factors[-1])
             self.channel_factors.append(n * self.channel_factors[-1])
@@ -32,7 +33,7 @@ class AudioToPose(nn.Module):
         channels = [factor for factor in self.channel_factors]
 
         self.audio_encoder = nn.ModuleList(
-            [ConvNormRelu1d(in_channels=channels[i], out_channels=channels[i + 1], leaky=True,
+            [[None, ConvNormRelu1d, ConvNormRelu2d][encoder_dim](in_channels=channels[i], out_channels=channels[i + 1], leaky=True,
                             downsample=(audio_encoder_mults[i] > 1), input_shape=sizes[i],
                             output_shape=sizes[i + 1]) for i in range(len(sizes) - 1)])
 
