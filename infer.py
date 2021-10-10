426919b0b61fb7272338e0c286d3da6ff3d4e120
Sarthak Kamat <sarthakkamat@gmail.com>
2021-10-09 21:10:47 -0700

    bug fixes

--- infer.py
+++ infer.py
@@ -2,6 +2,7 @@ import torch
 from model import AudioToPose, PoseDiscriminator
 from transforms import BVHtoMocapData, MocapDataToExpMap, Pipeline, AudioToLogMelSpec
 from pathlib import Path
+import librosa
 
 class GesturePrediction:
 
@@ -19,8 +20,7 @@ class GesturePrediction:
 
     def apply(self, audio_file, bvh_file):
         real_pose = torch.from_numpy(self.mocap_pipeline.apply(bvh_file)).unsqueeze(0).cuda()
-        audio_encoding = self.mel_spec.apply(librosa.load(audio_file, offset=t,
-                                                          duration=self.clip_duration, mono=True)).unsqueeze(0).cuda()
+        audio_encoding = torch.from_numpy(self.mel_spec.apply(librosa.load(audio_file, mono=True))).unsqueeze(0).cuda()
         if self.generator is None or audio_encoding.shape[-2:] != self.last_audio_shape:
             self.generator = AudioToPose(input_shape=audio_encoding.shape[-2:], pose_shape=real_pose.shape[-2:],
                                          encoder_dim=args.encoder_dim)
